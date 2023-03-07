3b39b42f91b9d10a7306f19b54ac1b1ace3fae7b
Artyom <exampe@mail.com>
2023-03-07 17:44:07 +0200

    customization of a simple console application

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,12 +10,27 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-  b = prompt('На сколько оцените его?', ''),
-  c = prompt('Один из последних просмотренных фильмов?', ''),
-  d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('Done!');
+  } else {
+    console.log('Error!');
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  alert('Просмотрено довольно мало фильмов! 😞');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert('Вы классический зритель! 🙄');
+} else if (personalMovieDB.count >= 30) {
+  alert('Вы киноман! 😀');
+} else {
+  alert('Произошла ошибка!');
+}
 
 console.log(personalMovieDB);
